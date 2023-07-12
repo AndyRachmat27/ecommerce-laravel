9c372e078c427346bcd6a993861db35e1a95f733
AndyRachmat27 <andyrachmat12@gmail.com>
2023-07-12 12:30:51 +0700

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="bg-dark">
+    <div class="container bg-dark">
+        <div class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="footer-link">Privacy policy</a><br>
+                    <a href="#" class="footer-link">Term and conditions</a><br>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Welcome to the site
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, assumenda. Culpa, libero.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Contact:
+                    </h3>
+                    <p>Telepon : 0819-0890-9452</p>
+                    <p>Website: <a href="https://greencloth.com">Personal Website</a></p>
+                    <p>Tokopedia: <a href="https://www.tokopedia.com/greencloth">Green Cloth</a></p>
+                </div>
+            </div>
+        </div>
+        <p>copyright &copy; All rights reserved 2023</p>
+    </div>
+</footer>
